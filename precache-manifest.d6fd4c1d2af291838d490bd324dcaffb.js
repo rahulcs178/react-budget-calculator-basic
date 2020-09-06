c69e5222d168e99fe582fa1fd7b571a12a9fcb6e
Rahul Mishra <rahulmishra20178@yahoo.in>
2020-09-06 14:14:00 +0530

    Updates

---
+++ precache-manifest.d6fd4c1d2af291838d490bd324dcaffb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828448c06c570675dae3f22d339c9fdc",
+    "url": "/react-budget-calculator-basic/index.html"
+  },
+  {
+    "revision": "825e463dd82e40b97f30",
+    "url": "/react-budget-calculator-basic/static/css/main.aca86320.chunk.css"
+  },
+  {
+    "revision": "07afbe2b06114fe43571",
+    "url": "/react-budget-calculator-basic/static/js/2.cb963acc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-budget-calculator-basic/static/js/2.cb963acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825e463dd82e40b97f30",
+    "url": "/react-budget-calculator-basic/static/js/main.1bdf2725.chunk.js"
+  },
+  {
+    "revision": "686381c01fa09e5c1699",
+    "url": "/react-budget-calculator-basic/static/js/runtime-main.3c595be4.js"
+  }
+]);
